93f0ab0c9c4d073c3f4338855283943ed08a6309
Jiehao <jiehaoji530@gmail.com>
2026-01-12 15:51:57 +0100

    Get Followers with a follower_id result

--- app/helpers/follow_helper.py
+++ app/helpers/follow_helper.py
@@ -1,12 +1,18 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from services.database import follow_collection
 from models.follow_model import Follow
 from schemes.follow_scheme import follow_scheme
 
-async def search_follow(field: str, value, field_2: str, value_2):
-    follow = await follow_collection.find_one({field: value, field_2: value_2})
+async def verify_follow(field: str, value, field_2: str, value_2):
+    verify = await follow_collection.find_one({field: value, field_2: value_2})
+    if not verify:
+        return None
+    return Follow(**follow_scheme(verify))
+
+async def search_follow(field: str, value):
+    follow = await follow_collection.find({field: value}).to_list(1000)
     if not follow:
         return None
-    return Follow(**follow_scheme(follow))
+    return [follow_scheme(data) for data in follow]
--- app/routers/follow.py
+++ app/routers/follow.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from fastapi import APIRouter, status, HTTPException, Depends
-from helpers.follow_helper import search_follow
+from helpers.follow_helper import verify_follow, search_follow
 from helpers.users_helper import verify_token
 from models.follow_model import Follow
 from models.users_model import User
@@ -13,7 +13,7 @@ router = APIRouter(tags=["follow"])
 
 @router.post("/profile/{followed_id}/follow", status_code=status.HTTP_202_ACCEPTED)
 async def follow_user(followed_id: str, user: User = Depends(verify_token)):
-    search = await search_follow("follower_id", ObjectId(user.id), "follower_id", ObjectId(followed_id))
+    search = await verify_follow("follower_id", ObjectId(user.id), "followed_id", ObjectId(followed_id))
     if isinstance(search, Follow):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are already following this user")
     
@@ -28,7 +28,7 @@ async def follow_user(followed_id: str, user: User = Depends(verify_token)):
 
 @router.delete("/profile/{unfollow_id}/unfollow", status_code=status.HTTP_202_ACCEPTED)
 async def unfollow_user(unfollow_id: str, user: User = Depends(verify_token)):
-    search = await search_follow("follower_id", ObjectId(user.id), "followed_id", ObjectId(unfollow_id))
+    search = await verify_follow("follower_id", ObjectId(user.id), "followed_id", ObjectId(unfollow_id))
     if not isinstance(search, Follow):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You aren't following this user")
 
@@ -36,3 +36,10 @@ async def unfollow_user(unfollow_id: str, user: User = Depends(verify_token)):
     if unfollow.deleted_count == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Unfollow Error")
     return {"detail": f"Unfollowed"}
+
+@router.get("/profile/{user_id}/followers", status_code=status.HTTP_202_ACCEPTED)
+async def get_followers(user_id: str):
+    search = await search_follow("followed_id", ObjectId(user_id))
+    if search is None:
+        return []
+    return [follower["follower_id"] for follower in search]
