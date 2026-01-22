03c42b3019a8ce8f36d2f66e6ae2b6076812138e
Jiehao <jiehaoji530@gmail.com>
2026-01-22 18:51:18 +0100

    Query Parameter added to Get Your Pokemon Figures

--- app/helpers/pokemon_helper.py
+++ app/helpers/pokemon_helper.py
@@ -5,6 +5,7 @@ from services.database import pokemon_collection
 from models.pokemon_model import Pokemon, Rarity
 from schemes.pokemon_scheme import pokemon_scheme
 import random
+from bson import ObjectId
 
 async def search_pokemon(number: int):
     pokemon = await pokemon_collection.find_one({"number": number})
@@ -21,6 +22,23 @@ def get_rarity_and_point_pokemon():
     point = random.choices(point_options, weights=point_weights, k=1)[0]
     return [rarity, point]
 
+def get_search_parameters(user_id: str, number, name, type, generation, rarity, points):
+    search_parameters = {"user_id": ObjectId(user_id)}
+
+    if number is not None:
+        search_parameters["pokemon_figure.number"] = number
+    if name is not None:
+        search_parameters["pokemon_figure.type"] = name
+    if type is not None:
+        search_parameters["pokemon_figure.type"] = {"$in": type}
+    if generation is not None:
+        search_parameters["pokemon_figure.generation"] = generation
+    if rarity is not None:
+        search_parameters["pokemon_figure.rarity"] = rarity
+    if points is not None:
+        search_parameters["pokemon_figure.points"] = points
+    
+    return search_parameters
 
 
 
--- app/routers/pokemon.py
+++ app/routers/pokemon.py
@@ -4,18 +4,32 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from fastapi import APIRouter, status, HTTPException, Depends, Query
 from services.database import pokemon_figure_collection
 from schemes.pokemon_scheme import pokemon_figure_scheme
+from helpers.pokemon_helper import get_search_parameters
 from models.users_model import User
 from helpers.users_helper import verify_token
 from bson import ObjectId
+from typing import Optional, List
 
 router = APIRouter(tags=["pokémon"])
 
 @router.get("/pokemon_figures", status_code=status.HTTP_202_ACCEPTED)
-async def get_your_pokemon_figures(page: int = Query(1, ge=1), pagesize: int = Query(24, ge=1, le=48), user: User = Depends(verify_token)):
-    total_pokemon_figures = await pokemon_figure_collection.count_documents({"user_id": ObjectId(user.id)})
+async def get_your_pokemon_figures(
+    page: int = Query(1, ge=1, description="Obligation"), 
+    pagesize: int = Query(24, ge=1, le=48, description="Obligation"),
+    number: Optional[int] = Query(None, ge=1, description="Optional"),
+    name: Optional[str] = Query(None, description="Optional"),
+    type: Optional[List[str]] = Query(None, description="Optional"),
+    generation: Optional[int] = Query(None, ge=1, le=3, description="Optional"),
+    rarity: Optional[str] = Query(None, description="Optional"),
+    points: Optional[int] = Query(None, ge=1, le=5, description="Optional"),
+    user: User = Depends(verify_token)
+    ):
+    search_parameters = get_search_parameters(user.id, number, name, type, generation, rarity, points)
+    
+    total_pokemon_figures = await pokemon_figure_collection.count_documents(search_parameters)
     
     skip = (page - 1) * pagesize
-    pokemon_figures = await pokemon_figure_collection.find({"user_id": ObjectId(user.id)}).skip(skip).limit(pagesize).to_list()
+    pokemon_figures = await pokemon_figure_collection.find(search_parameters).skip(skip).limit(pagesize).to_list()
     if not pokemon_figures:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     list_pokemon_figures = [pokemon_figure_scheme(pokemon_figure) for pokemon_figure in pokemon_figures]
