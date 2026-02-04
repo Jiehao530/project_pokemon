d4c12698cf0f740192ab34375ac4e49afd8e1683
Jiehao <jiehaoji530@gmail.com>
2026-02-04 16:53:52 +0100

    Assigning IDs to the Pokemon Figures and separating them into different documents in the shop_pokemon_figures collection

--- app/helpers/shop_helper.py
+++ app/helpers/shop_helper.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from fastapi import HTTPException, status
 from services.database import shop_status_collection, shop_pokemon_figures_collection
 from helpers.pokemon_helper import search_pokemon, get_rarity_and_point_pokemon, get_price_for_pokemon_figure
-from models.shop_model import ShopStatusDB, ShopPokemonFigures
+from models.shop_model import ShopStatusDB
 from schemes.shop_scheme import shop_status_scheme, shop_pokemon_figures_scheme
 from manager.shop_manager import ShopManager
 from bson import ObjectId
@@ -21,7 +21,6 @@ async def get_pokemon_figure_for_shop(number_repetition: int):
         rarity, points =  get_rarity_and_point_pokemon()
         price = get_price_for_pokemon_figure(rarity, points)
         pokemon_figure = {
-            "pokemon_figure_id": ObjectId(),
             "number": pokemon.number,
             "name": pokemon.name,
             "type": pokemon.type,
@@ -40,18 +39,21 @@ async def search_shop_status(field: str, value):
         return None
     return ShopStatusDB(**shop_status_scheme(shop_status))
 
-async def search_shop_pokemon_figures(field: str, value):
-    shop_pokemon_figures = await shop_pokemon_figures_collection.find_one({field: value})
-    if not shop_pokemon_figures:
-        return None
-    return ShopPokemonFigures(**shop_pokemon_figures_scheme(shop_pokemon_figures))
+async def search_pokemon_figure_for_shop(shop_id: str):
+    pokemon_figures = await shop_pokemon_figures_collection.find({"shop_id": ObjectId(shop_id)}).to_list()
+    if pokemon_figures is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pokemon figures for shop not found")
+    return shop_pokemon_figures_scheme(pokemon_figures)
 
-async def update_pokemon_figures_for_shop(number_repetition: int, shop_id: str):
-    new_pokemon_figures = await get_pokemon_figure_for_shop(number_repetition)
-    update_data = {
-        "pokemon_figures": new_pokemon_figures
-    }
-    await shop_pokemon_figures_collection.update_one({"shop_id": ObjectId(shop_id)}, {"$set": update_data})
+async def update_pokemon_figure_for_shop(number_repetition: int, shop_id: str):
+    await shop_pokemon_figures_collection.delete_many({"shop_id": ObjectId(shop_id)})
+    pokemon_figures_list = await get_pokemon_figure_for_shop(number_repetition)
+    for pokemon_figure in pokemon_figures_list:
+        shop_pokemon_figure = {
+            "shop_id": ObjectId(shop_id),
+            "pokemon_figure": pokemon_figure
+        }
+        await shop_pokemon_figures_collection.insert_one(shop_pokemon_figure)
 
 async def get_normal_shop_status(shop_name: str):
     shop_status = await search_shop_status("name", shop_name)
@@ -60,10 +62,9 @@ async def get_normal_shop_status(shop_name: str):
         
     normal_shop = ShopManager(shop_status.last_generated, shop_status.next_shop)
     update_data = normal_shop.update_shop_status()
-    model = model = normal_shop.model_shop_status()
+    model = normal_shop.model_shop_status()
 
     if update_data:
         await shop_status_collection.update_one({"name": shop_name}, {"$set": model.model_dump()})
-        await update_pokemon_figures_for_shop(3, shop_status.id)
-    
+        await update_pokemon_figure_for_shop(3, shop_status.id)
     return model
--- app/models/shop_model.py
+++ app/models/shop_model.py
@@ -3,8 +3,6 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from pydantic import BaseModel
 from datetime import datetime
-from typing import List
-from models.pokemon_model import PokemonFigureForSale
 
 class ShopStatus(BaseModel):
     last_generated: datetime
@@ -12,8 +10,3 @@ class ShopStatus(BaseModel):
 
 class ShopStatusDB(ShopStatus):
     id: str
-    
-class ShopPokemonFigures(BaseModel):
-    shop_id: str
-    shop_name: str
-    pokemon_figures: List[PokemonFigureForSale]
