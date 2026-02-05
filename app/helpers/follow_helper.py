253ae84f5b5434c86a0acfa11e62d42df7bec27f
Jiehao <jiehaoji530@gmail.com>
2026-02-05 16:17:30 +0100

    Adding comments in Follow

--- app/helpers/follow_helper.py
+++ app/helpers/follow_helper.py
@@ -6,27 +6,77 @@ from models.follow_model import Follow
 from schemes.follow_scheme import follow_scheme
 from bson import ObjectId
 
+#Función para obtener el documento de seguir (2 parámetros)
 async def verify_follow(field: str, value, field_2: str, value_2):
+    """
+    Obtener seguir
+    ---------------------------------
+    Flujo:
+    1. Busca el documento de seguir
+    2. Retorna el documento
+
+    Respuestas:
+    - Retorna None
+    - Retorna Follow
+    """
     verify = await follow_collection.find_one({field: value, field_2: value_2})
     if not verify:
         return None
     return Follow(**follow_scheme(verify))
 
+#Función para obtener la lista de documentos de seguir
 async def search_follow(field: str, value):
+    """
+    Obtener lista de documentos
+    ---------------------------------
+    Flujo:
+    1. Busca todos los documentos de seguir que coincidan con los parámetros
+    2. Retorna la lista de documentos
+
+    Respuestas:
+    - Retorna None
+    - Retorna una lista
+    """
     follow = await follow_collection.find({field: value}).to_list(1000)
     if not follow:
         return None
     return [follow_scheme(data) for data in follow]
 
+#Función para obtener el número de seguidores
 async def get_followers_numbers(user_id: str):
+    """
+    Obtener número de seguidores
+    ---------------------------------
+    Flujo:
+    1. Busca todos los documentos de seguir
+    2. Retorna la cantidad de documentos que ha encontrado
+
+    Respuestas:
+    - Retorna 0
+    - Retorna followers_numbers
+    """
     search = await search_follow("followed_id", ObjectId(user_id))
+    #Si no hay documentos quiere decir que no hay seguidores por eso 0
     if search is None:
         return 0
     followers_numbers = len(search)
     return followers_numbers
 
+#Función para obtener el número de siguiendo
 async def get_following_numbers(user_id: str):
+    """
+    Obtener número de siguiendo
+    ---------------------------------
+    Flujo:
+    1. Busca todos los documentos de siguiendo
+    2. Retorna la cantidad de documentos que ha encontrado
+
+    Respuestas:
+    - Retorna 0
+    - Retorna following_numbers
+    """
     search = await search_follow("follower_id", ObjectId(user_id))
+    #Si no hay documentos quiere decir que no sigue a nadie por eso 0
     if search is None:
         return 0
     following_numbers = len(search)
--- app/routers/follow.py
+++ app/routers/follow.py
@@ -11,9 +11,27 @@ from bson import ObjectId
 
 router = APIRouter(tags=["follow"])
 
+#Endpoint para seguir a un usuario
 @router.post("/profile/{followed_id}/follow", status_code=status.HTTP_202_ACCEPTED)
 async def follow_user(followed_id: str, user: User = Depends(verify_token)):
+    """
+    Seguir usuario
+    --------------------------------------------------------------
+    Flujo:
+    1. Convierte el followed_id en un ObjectID
+    2. Busca si ya estaba siguiendo el usuario
+    3. Inserta los datos
+    4. Retorna que lo ha seguido
+
+    Respuestas:
+    - 202: Se ha seguido
+    - 400: Ya lo estaba siguiendo
+    - 400: Error al seguir
+    - Otros: Consultar las funciones verify_user_id y verify_token en users_helpers
+    """
+    #Al convertir en ObjectID se puede saber si es válido o inválido
     objectid_followed_id = verify_user_id(followed_id)
+    #Al buscar si lo estaba siguiendo, hay que buscar el documento que contengan estas coincidencias
     search = await verify_follow("follower_id", ObjectId(user.id), "followed_id", objectid_followed_id)
     if isinstance(search, Follow):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are already following this user")
@@ -23,13 +41,31 @@ async def follow_user(followed_id: str, user: User = Depends(verify_token)):
         "followed_id": objectid_followed_id
     }
     insert_follow_data = await follow_collection.insert_one(follow_data)
+    #Manteja error si el documento no es correcto
     if not insert_follow_data.acknowledged:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Follow Error")
     return {"detail": f"Followed"}
 
+#Endpoint para dejar de seguir a un usuario
 @router.delete("/profile/{unfollow_id}/unfollow", status_code=status.HTTP_202_ACCEPTED)
 async def unfollow_user(unfollow_id: str, user: User = Depends(verify_token)):
+    """
+    Dejar de seguir
+    ----------------------------------
+    Flujo:
+    1. Convierte el unfollow_id en un ObjectID
+    2. Busca si no estaba siguiendo el usuario
+    3. Elimina el documento
+    4. Retorna que lo ha dejado de seguir
+
+    Respuestas:
+    - 202: Se ha dejado de seguir
+    - 400: No lo estaba siguiendo
+    - 400: Error al dejar de seguir
+    - Otros: Consultar las funciones verify_user_id y verify_token en users_helpers
+    """
     objectid_unfollow_id = verify_user_id(unfollow_id)
+    #Hay que verificar si no lo estaba siguiendo para controlar el error
     search = await verify_follow("follower_id", ObjectId(user.id), "followed_id", objectid_unfollow_id)
     if search is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You aren't following this user")
@@ -39,24 +75,56 @@ async def unfollow_user(unfollow_id: str, user: User = Depends(verify_token)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Unfollow Error")
     return {"detail": f"Unfollowed"}
 
+#Endpoint para obtener lista de seguidores
 @router.get("/profile/{user_id}/followers", status_code=status.HTTP_202_ACCEPTED)
 async def get_followers(user_id: str):
+    """
+    Obtener seguidores
+    ----------------------------------
+    Flujo:
+    1. Convierte el user_id en un ObjectID
+    2. Busca los seguidores
+    3. Obtiene los seguidores
+
+    Respuestas:
+    - 202: Se ha obtenido los seguidores
+    - Otros: Consultar la función verify_user_id
+    """
     objectid_user_id = verify_user_id(user_id)
     search = await search_follow("followed_id", objectid_user_id)
+    #Si no hay seguidores hay que retornar una lista vacía
     if search is None:
         return {"followers": []}
+    #Hay que hacer una nueva lista que contenga solo los ID de los seguidores y convertirlos en ObjectID para luego poder buscarlos
     followers_id_list = [ObjectId(follower_id["follower_id"]) for follower_id in search]
     followers_profiles = await profile_collection.find({"user_id": {"$in": followers_id_list}}).to_list(1000)
+    #Hacer una nueva lista con solo los nombres de usuario de los seguidores
     followers = [follower_username["username"] for follower_username in followers_profiles]
     return {"followers": followers}
 
+#Endpoint para obtener lista de siguiendo
 @router.get("/profile/{user_id}/following", status_code=status.HTTP_202_ACCEPTED)
 async def get_following(user_id: str):
+    """
+    Obtener siguiendo
+    ----------------------------------
+    Flujo:
+    1. Convierte el user_id en un ObjectID
+    2. Busca los usuarios que sigue
+    3. Obtiene los usuarios que sigue
+
+    Respuestas:
+    - 202: Se ha obtenido los seguidores
+    - Otros: Consultar la función verify_user_id
+    """
     objectid_user_id = verify_user_id(user_id)
     search = await search_follow("follower_id", objectid_user_id)
+    #Si no sigue a nadie hay que retornar una lista vacía
     if search is None:
         return {"following": []}
+    #Hay que hacer una nueva lista que contenga solo los IDs de usuario que sigue y convertirlos en ObjectID para luego poder buscarlos
     following_id_list = [ObjectId(followed_id["followed_id"]) for followed_id in search]
     following_profiles = await profile_collection.find({"user_id": {"$in": following_id_list}}).to_list(1000)
+    #Hacer una nueva lista con solo los nombres de usuario que sigue
     following = [following_username["username"] for following_username in following_profiles]
     return {"following": following}
