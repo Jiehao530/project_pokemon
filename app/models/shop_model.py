75ae3d3755949c2dd16f40669e5b45d84b04699f
Jiehao <jiehaoji530@gmail.com>
2026-02-07 15:01:44 +0100

    New Models

--- app/models/shop_model.py
+++ app/models/shop_model.py
@@ -3,6 +3,16 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from pydantic import BaseModel
 from datetime import datetime
+from enum import Enum
+from models.pokemon_model import PokemonFigureShop
+
+class ShopType(str, Enum):
+    POKEMON_FIGURE = "pokemon_figure"
+    POKECOINS = "pokecoins"
+
+class Currency(str, Enum):
+    POKECOINS = "pokecoins"
+    REAL_MONEY = "real_money"
 
 class ShopStatus(BaseModel):
     last_generated: datetime
@@ -10,3 +20,18 @@ class ShopStatus(BaseModel):
 
 class ShopStatusDB(ShopStatus):
     id: str
+
+class ShopPokemonFigures(BaseModel):
+    id: str
+    type: ShopType
+    pokemon_figure: PokemonFigureShop
+    price: int
+    currency: Currency
+    expires_at: int
+
+class ShopPokecoins(BaseModel):
+    id: str
+    type: ShopType
+    item_id: str
+    price: float
+    currency: Currency
