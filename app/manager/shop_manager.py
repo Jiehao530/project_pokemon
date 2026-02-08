de4ce1b2e9968132eeea3d2d115e83dc4da990d3
Jiehao <jiehaoji530@gmail.com>
2026-02-08 11:37:36 +0100

    Redoing the Shop Manager

--- app/manager/shop_manager.py
+++ app/manager/shop_manager.py
@@ -2,31 +2,35 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from datetime import datetime, timedelta
-from models.shop_model import ShopStatus
 
 class ShopManager:
-    UPDATE_IN = timedelta(days=1)
+    def __init__(self, refresh_at: datetime, last_refresh: datetime, refresh_interval_hours: int):
+        self.refresh_at = refresh_at
+        self.last_refresh = last_refresh
+        self.refresh_interval_hours = refresh_interval_hours
 
-    def __init__(self, last_generated: datetime, next_shop: datetime):
-        self.last_generated = last_generated
-        self.next_shop = next_shop
-    
-    def update_shop_status(self):
+    def update_shop_refresh(self):
         now = datetime.utcnow()
-        if not now >= self.next_shop:
+        interval_hours = timedelta(hours=self.refresh_interval_hours)
+        
+        if not now >= self.refresh_at:
             return False
-        time_elapsed = now - self.last_generated
-        days_elapsed = time_elapsed // self.UPDATE_IN
-        days = timedelta(days=days_elapsed)
-        
-        last_update = self.last_generated + days
-        next_update = last_update + self.UPDATE_IN
-        
-        self.last_generated = last_update
-        self.next_shop = next_update
+
+        time_elapsed = now - self.last_refresh
+        total_refresh_intervals = (time_elapsed // interval_hours) * interval_hours
+
+        update_last_refresh = self.last_refresh + total_refresh_intervals
+        update_next_refresh = update_last_refresh + interval_hours
+
+        self.last_refresh = update_last_refresh
+        self.refresh_at = update_next_refresh
+
         return True
-        
-    def model_shop_status(self) -> ShopStatus:
-        return ShopStatus(last_generated=self.last_generated, next_shop=self.next_shop)
-    
-         
+
+    def get_data(self) -> dict:
+        data_dict = {
+            "refresh_at": self.refresh_at,
+            "last_refresh": self.last_refresh,
+            "refresh_interval_hours": self.refresh_interval_hours
+        }
+        return data_dict
