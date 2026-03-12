012c1db63997398bfa7a86de9851c504324a865a
Jiehao <jiehaoji530@gmail.com>
2026-03-12 21:40:58 +0100

    Bug Fixes 2

--- app/services/ws_service.py
+++ app/services/ws_service.py
@@ -1,7 +1,8 @@
-from fastapi import WebSocket
+from fastapi import WebSocket, WebSocketDisconnect
 from app.dependencies.ws_auth_depends import get_current_user_for_ws
 from app.memory.exchange_rooms_memory import EXCHANGE_ROOMS
 from app.enums.exchange_type_enum import ExchangeType
+from app.enums.role_for_exchange_enum import ExchangeRole
 from app.schemes.users_scheme import User
 from uuid import uuid4
 
@@ -19,27 +20,27 @@ class WedsocketService:
         return {"exchange_id": exchange_id}
     
     async def exchange_ws(self, wedsocket: WebSocket, exchange_id: str):
-        await WebSocket.accept()
+        await wedsocket.accept()
         exchange_room = EXCHANGE_ROOMS.get(exchange_id)
         if not exchange_room:
-            wedsocket.send_json({ExchangeType.ERROR.value: "Exchange Room Not Found"})
-            wedsocket.close()
+            await wedsocket.send_json({ExchangeType.ERROR.value: "Exchange Room Not Found"})
+            await wedsocket.close()
             return
 
         auth_header = wedsocket.headers.get("authorization")
         if not auth_header:
-            wedsocket.send_json({ExchangeType.ERROR.value: "Missing Authorization Header"})
-            wedsocket.close()
+            await wedsocket.send_json({ExchangeType.ERROR.value: "Missing Authorization Header"})
+            await wedsocket.close()
             return
         
         token_type, _ , token = auth_header.partition(" ")
         if token_type.lower() != "bearer":
-            wedsocket.send_json({ExchangeType.ERROR.value: "Incorrect Token Type"})
-            wedsocket.close()
+            await wedsocket.send_json({ExchangeType.ERROR.value: "Incorrect Token Type"})
+            await wedsocket.close()
             return
         
         user = await get_current_user_for_ws(token)
         if not user:
-            wedsocket.send_json({ExchangeType.ERROR.value: "Invalid Token"})
-            wedsocket.close()
+            await wedsocket.send_json({ExchangeType.ERROR.value: "Invalid Token"})
+            await wedsocket.close()
             return
